2170b9bc28bc4ca40d0c39d0bcdb5e9f85c2c4a2
Rukayat Jimoh <jimohadedamola925@gmail.com>
2024-05-24 16:21:32 -0400

    Added a dotenv file to the code

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 import lxml
 from smtplib import SMTP
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 AMAZON_URL = "https://www.amazon.ca/SodaStream-Terra-Sparkling-Water-Bundle/dp/B0C2QRCN7Y/?_encoding=UTF8&pd_rd_w=hfH3i&content-id=amzn1.sym.5758671b-686d-4d7c-af22-08ab8e3c615f%3Aamzn1.symc.d10b1e54-47e4-4b2a-b42d-92fe6ebbe579&pf_rd_p=5758671b-686d-4d7c-af22-08ab8e3c615f&pf_rd_r=GD6Y3V71HWP2RMH3V2D1&pd_rd_wg=NeZEf&pd_rd_r=9679f6be-fc4a-4367-a0ce-01ae2e5ffb70&ref_=pd_gw_ci_mcx_mr_hp_atf_m&th=1"
 HEADER = {
@@ -17,8 +21,8 @@ price = float(soup.find(name="span", class_="aok-offscreen").getText().split()[0
 print(price)
 
 if price <= 100:
-    my_email = "pythonsmtptest282@gmail.com"
-    password = "hnzdpcyrkbiyxiyq"
+    my_email = os.getenv("EMAIL")
+    password = os.getenv("PASSWORD")
     with SMTP("smtp.gmail.com", port=587) as connection:
         connection.starttls()
         connection.login(user=my_email, password=password)
